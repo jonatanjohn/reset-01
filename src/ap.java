0f520223874af90ef65b9cddd68d0cc836c81a64
Reset <Reset@NB-004241.cwinet.local>
2020-03-07 10:55:25 -0300

    /criação do projeto

---
+++ src/ap.java
@@ -0,0 +1,9 @@
+public class ap {
+    public static void main(String [] args) {
+        System.out.println("Hello World");
+        System.out.println("Outras coisas");
+        System.out.println("Sem criatividade");
+        System.out.println("pra mensagens");
+
+    }
+}
